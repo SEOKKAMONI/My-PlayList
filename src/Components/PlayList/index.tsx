c7f98bb91602b5871b29704aa57b02fd247134ab
SEOKKAMONI <sj01048260279@gmail.com>
2022-12-29 11:38:16 +0900

    ADD :: 유튜브 썸네일 추출해서 이미지 추가

--- src/Components/PlayList/index.tsx
+++ src/Components/PlayList/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import * as S from './style';
-import Thumbnail from '../../Assets/Img/thumbnail.png';
 
 interface PropsType {
   title: string;
@@ -10,12 +9,27 @@ interface PropsType {
 }
 
 export default function PlayList({ title, explain, url, name }: PropsType) {
+  let replaceUrl: string;
+  let finUrl: string[];
+
+  const ExportThumnail = (youtubeUrl: string) => {
+    replaceUrl = youtubeUrl.replace('https://youtu.be/', '');
+    replaceUrl = youtubeUrl.replace('https://www.youtube.com/embed/', '');
+    replaceUrl = youtubeUrl.replace('https://www.youtube.com/watch?v=', '');
+    finUrl = replaceUrl.split('&');
+    return finUrl[0];
+  };
+
   return (
     <S.PlayList>
       <S.ImgBox>
         <S.Link href={url} target="_blank">
           <S.Shortcut>바로가기</S.Shortcut>
-          <S.Img src={Thumbnail} />
+          <S.Img
+            src={`https://img.youtube.com/vi/${ExportThumnail(
+              url,
+            )}/mqdefault.jpg`}
+          />
         </S.Link>
       </S.ImgBox>
       <S.Contents>
